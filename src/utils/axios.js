c754a12faa3b81f8a67e97485e6547ad55483666
Shriraj26 <89901228+ShrirajSDSU@users.noreply.github.com>
2023-07-01 12:46:58 -0700

    made changes

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `https://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`,
+  baseURL: `https://api.weatherapi.com/v1/forecast.json?key=8ba41d1e2d934150b6b204730233006`,
   timeout: 1000,
 });
 
